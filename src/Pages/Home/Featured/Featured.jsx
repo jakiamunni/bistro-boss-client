c5b5841c153d3df4c216ceafd29eb0dbe22560ba
jakiamunni <jakiasmunni205@gmail.com>
2023-09-15 22:10:37 +0600

    features added

--- src/Pages/Home/Featured/Featured.jsx
+++ src/Pages/Home/Featured/Featured.jsx
@@ -1,23 +1,27 @@
 import SectionTitle from "../../../components/SectionTitlee/SectionTitle";
 import featuredImage from '../../../assets/home/featured.jpg';
+import './Featured.css';
 
 const Featured = () => {
     return (
-        <div>
+        <div className="featured-item bg-fixed text-white pt-8 my-20">
             <SectionTitle 
               subHeading= "check it out"
               heading= "featured item"
             ></SectionTitle>
-            <div>
+            <div className="md:flex justify-center bg-slate-500 bg-opacity-40 items-center pb-20 pt-12 px-36">
+            <div className="">
                 <img src={featuredImage} alt="" />
             </div>
-            <div>
+            <div className="md:ml-10">
                 <p>Aug 20, 2023</p>
                 <p className="uppercase">Where can i get some?</p>
                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia architecto enim id quaerat, impedit minus nostrum modi quia dolorum! Fuga mollitia rem officia repellendus similique esse atque. Sunt, libero sequi. Repellendus ducimus nemo dolore non tenetur cum ea! Reiciendis velit ex ullam iure necessitatibus consequuntur nam eum delectus, in dolore.
                  </p>
+                 <button className="btn btn-outline border-0 border-b-4 mt-4">Order Now</button>
             </div>
         </div>
+            </div>
     );
 };
 
